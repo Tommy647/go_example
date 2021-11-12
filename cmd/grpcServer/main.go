8bab2afa1a9100b1c8f680411b6b5fb593d5fad2
Tom Smith <tom@t-smith.co.uk>
2021-11-12 20:11:48 +0000

    working vault on docker/start

--- cmd/grpcServer/main.go
+++ cmd/grpcServer/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net"
 	"os"
 	"strings"
 
+	vault "github.com/hashicorp/vault/api"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 
@@ -22,6 +24,9 @@ const (
 )
 
 func main() {
+	// connect to vault?
+	_, _ = getSecretCert()
+
 	// create a tcp listener for your gRPC service
 	listener, err := net.Listen("tcp", "0.0.0.0:9090") //nolint:gosec // development use only
 	if err != nil {
@@ -52,4 +57,53 @@ func main() {
 	log.Print("starting grpcServer")
 	// serve the grpc server on the tcp listener - this blocks until told to close
 	log.Fatal(gRPCServer.Serve(listener))
+}
+
+func getSecretCert() (string, error) {
+	config := vault.DefaultConfig()
+
+	client, err := vault.NewClient(config)
+	if err != nil {
+		return "", err
+	}
+
+	jwt, err := os.ReadFile("/etc/vault/service_account")
+	if err != nil {
+		return "", err
+	}
+	params := map[string]interface{}{
+		"jwt":  string(jwt),
+		"role": "dev-role-k8s", // the name of the role in Vault that was created with this app's Kubernetes service account bound to it
+	}
+
+	// log in to Vault's Kubernetes auth method
+	resp, err := client.Logical().Write("auth/kubernetes/login", params)
+	if err != nil {
+		return "", fmt.Errorf("unable to log in with Kubernetes auth: %w", err)
+	}
+	if resp == nil || resp.Auth == nil || resp.Auth.ClientToken == "" {
+		return "", fmt.Errorf("login response did not return client token")
+	}
+	// now you will use the resulting Vault token for making all future calls to Vault
+	client.SetToken(resp.Auth.ClientToken)
+
+	// get secret from Vault
+	secret, err := client.Logical().Read("kv-v2/data/creds")
+	if err != nil {
+		return "", fmt.Errorf("unable to read secret: %w", err)
+	}
+
+	data, ok := secret.Data["data"].(map[string]interface{})
+	if !ok {
+		return "", fmt.Errorf("data type assertion failed: %T %#v", secret.Data["data"], secret.Data["data"])
+	}
+
+	// data map can contain more than one key-value pair, in this case we're just grabbing one of them
+	key := "password"
+	value, ok := data[key].(string)
+	if !ok {
+		return "", fmt.Errorf("value type assertion failed: %T %#v", data[key], data[key])
+	}
+
+	return value, nil
 }
