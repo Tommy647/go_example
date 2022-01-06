11149b3f4464372dd9c43f3434b4b8dc59843cf2
ferrier1 <james.ferrier01@gmail.com>
2022-01-06 09:32:09 +0000

    db methods complete

--- internal/db/db.go
+++ internal/db/db.go
@@ -3,6 +3,7 @@ package db
 import (
 	"database/sql"
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/Tommy647/go_example/internal/router"
@@ -13,18 +14,42 @@ type Store struct {
 }
 
 func (s Store) GetRouterByID(id string) (router.Router, error) {
-	//TODO implement me
-	panic("implement me")
+	var rtr router.Router
+
+	row := s.db.QueryRow(
+		`SELECT id, hostname, vendor, mgmtip FROM routers WHERE id=$1;`,
+		id,
+	)
+	err := row.Scan(&rtr.ID, &rtr.Hostname, &rtr.Vendor, &rtr.MgmtIP)
+	if err != nil {
+		log.Println(err.Error())
+		return router.Router{}, err
+	}
+	return rtr, nil
 }
 
-func (s Store) InsertRouter(router router.Router) (router.Router, error) {
-	//TODO implement me
-	panic("implement me")
+func (s Store) InsertRouter(rtr router.Router) (router.Router, error) {
+	_, err := s.db.Exec(
+		"INSERT INTO routers (id, hostname, vendor, mgmtip) VALUES ($1, $2, $3, $4)",
+		rtr.ID, rtr.Hostname, rtr.Vendor, rtr.MgmtIP,
+	)
+	if err != nil {
+		log.Println(err.Error())
+		return router.Router{}, err
+	}
+
+	return rtr, nil
 }
 
 func (s Store) DeleteRouter(id string) error {
-	//TODO implement me
-	panic("implement me")
+	_, err := s.db.Exec(
+		`DELETE FROM routers WHERE id=$1`, id,
+	)
+	if err != nil {
+		log.Println(err.Error())
+		return err
+	}
+	return nil
 }
 
 func New() (Store, error) {
